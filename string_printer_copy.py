b3e610d9fc3680b625ca1a317f658a655d9d2d8a
U-LAPTOP-ST6P16HD\pswan <peterswanton191@bellsouth.net>
2023-12-15 12:54:12 -0500

    h

--- string_printer_copy.py
+++
@@ -1,8 +0,0 @@
-import TextScripts
-from TextScripts import TextScripts, pokefirered_sym
-for symbol in pokefirered_sym:
-    string = symbol[3]
-    offset = symbol[0][2:]
-    offset_actual = symbol[0]
-    if string in TextScripts:
-        print(string + 'Portuguese 08' + offset)
