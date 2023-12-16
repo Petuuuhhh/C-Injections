54795a2d5fad3c3041a3128686b73b5066492a61
U-LAPTOP-ST6P16HD\pswan <peterswanton191@bellsouth.net>
2023-12-16 09:58:13 -0500

    Fucked

--- string_printer.py
+++ string_printer.py
@@ -1,8 +1,8 @@
 import TextScripts
-from TextScripts import TextScripts, pokefirered_sym
+from TextScripts import TextScripts, pokefirered_sym, Fucked
 for symbol in pokefirered_sym:
     string = symbol[3]
     offset = symbol[0][2:]
     offset_actual = symbol[0]
-    if string in TextScripts:
+    if string in TextScripts and string not in Fucked:
         print(string + ' 08' + offset)
