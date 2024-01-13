1d275bb2776a17b04e6f180b9e0dad81b0ec35b7
Petuuuhhh <peterswanton191@bellsouth.net>
2024-01-13 09:46:53 -0500

    Update to include more modern pokefirered include folder

--- include/gba/defines.h
+++ include/gba/defines.h
@@ -6,8 +6,19 @@
 #define TRUE  1
 #define FALSE 0
 
+#if defined(__APPLE__)
+#define IWRAM_DATA __attribute__((section("__DATA,iwram_data")))
+#define EWRAM_DATA __attribute__((section("__DATA,ewram_data")))
+#else
 #define IWRAM_DATA __attribute__((section("iwram_data")))
 #define EWRAM_DATA __attribute__((section("ewram_data")))
+#endif
+
+#if MODERN
+#define NOINLINE __attribute__((noinline))
+#else
+#define NOINLINE
+#endif
 
 #define ALIGNED(n) __attribute__((aligned(n)))
 
@@ -36,18 +47,20 @@
 #define BG_VRAM_SIZE      0x10000
 #define BG_CHAR_SIZE      0x4000
 #define BG_SCREEN_SIZE    0x800
-#define BG_CHAR_ADDR(n)   (BG_VRAM + (BG_CHAR_SIZE * (n)))
-#define BG_SCREEN_ADDR(n) (BG_VRAM + (BG_SCREEN_SIZE * (n)))
+#define BG_CHAR_ADDR(n)   (void *)(BG_VRAM + (BG_CHAR_SIZE * (n)))
+#define BG_SCREEN_ADDR(n) (void *)(BG_VRAM + (BG_SCREEN_SIZE * (n)))
+#define BG_TILE_ADDR(n)   (void *)(BG_VRAM + (0x80 * (n)))
 
-#define BG_TILE_H_FLIP(n) (0x400 + (n))
-#define BG_TILE_V_FLIP(n) (0x800 + (n))
+#define BG_TILE_H_FLIP(n)   (0x400 + (n))
+#define BG_TILE_V_FLIP(n)   (0x800 + (n))
+#define BG_TILE_H_V_FLIP(n) (0xC00 + (n))
 
 // text-mode BG
-#define OBJ_VRAM0      (VRAM + 0x10000)
+#define OBJ_VRAM0      (void *)(VRAM + 0x10000)
 #define OBJ_VRAM0_SIZE 0x8000
 
 // bitmap-mode BG
-#define OBJ_VRAM1      (VRAM + 0x14000)
+#define OBJ_VRAM1      (void *)(VRAM + 0x14000)
 #define OBJ_VRAM1_SIZE 0x4000
 
 #define OAM      0x7000000
@@ -65,5 +78,9 @@
 #define TILE_OFFSET_8BPP(n) ((n) * TILE_SIZE_8BPP)
 
 #define TOTAL_OBJ_TILE_COUNT 1024
+
+// Some functions are strictly inline asm
+#define NAKED __attribute__((naked))
+#define UNUSED __attribute__((unused))
 
 #endif // GUARD_GBA_DEFINES
