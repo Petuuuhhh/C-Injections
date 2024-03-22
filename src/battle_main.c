eb62188f94bb7b489d4c6c0f5bcf188ee3b55550
Petuuuhhh <peterswanton191@bellsouth.net>
2024-03-22 17:45:39 -0400

    Cool

--- src/battle_main.c
+++ src/battle_main.c
@@ -81,9 +81,28 @@ u8 CreateNPCTrainerPartyBadgeLevelScaling(struct Pokemon *party, u16 trainerNum)
                 const struct TrainerMonNoItemDefaultMoves *partyData = gTrainers[trainerNum].party.NoItemDefaultMoves;
                 u8 level = partyData[i].lvl;
                 u16 species = partyData[i].species;
-                if (badgeCount == 0 && gTrainerBattleOpponent_A == TRAINER_RIVAL_CHERRYGROVE_CITY_TOTODILE) level = 3;
-                if (gEvolutionTable[species][0].method == EVO_LEVEL && gEvolutionTable[species][0].param > level) species = gEvolutionTable[species][0].targetSpecies;
-
+                if (gTrainers[gTrainerBattleOpponent_A].trainerClass != TRAINER_CLASS_LEADER) {
+                    if (badgeCount == 0) level = (Random() % 4) + 2;
+                    else if (badgeCount == 1) level = (Random() % 6) + 8;
+                    else if (badgeCount == 2) level = (Random() % 6) + 13;
+                    else if (badgeCount == 3) level = (Random() % 6) + 18;
+                    else if (badgeCount == 4) level = (Random() % 6) + 23;
+                    else if (badgeCount == 5) level = (Random() % 6) + 28;
+                    else if (badgeCount == 6) level = (Random() % 6) + 33;
+                    else if (badgeCount == 7) level = (Random() % 6) + 38;
+                    else if (badgeCount == 8) level = (Random() % 6) + 43;
+                }
+                else {
+                    if (badgeCount == 0) level = (Random() % 6) + 10;
+                    if (badgeCount == 1) level = (Random() % 6) + 16;
+                    if (badgeCount == 2) level = (Random() % 6) + 21;
+                    if (badgeCount == 3) level = (Random() % 6) + 27;
+                    if (badgeCount == 4) level = (Random() % 6) + 32;
+                    if (badgeCount == 5) level = (Random() % 6) + 38;
+                    if (badgeCount == 6) level = (Random() % 6) + 43;
+                    if (badgeCount == 7) level = (Random() % 6) + 49;
+                }
+                if (gEvolutionTable[species][0].method == EVO_LEVEL && gEvolutionTable[species][0].param <= level) species = gEvolutionTable[species][0].targetSpecies;
                 for (j = 0; gSpeciesNames[partyData[i].species][j] != EOS; j++)
                     nameHash += gSpeciesNames[partyData[i].species][j];
 
--- src/wild_encounter.c
+++ src/wild_encounter.c
@@ -17,6 +17,8 @@
 #include "../include/constants/abilities.h"
 #include "../include/constants/items.h"
 
+extern struct Evolution gEvolutionTable[][EVOS_PER_MON];
+
 enum
 {
     WILD_AREA_LAND,
@@ -33,6 +35,7 @@ bool8 TryGenerateWildMonLevelScaling(const struct WildPokemonInfo * info, u8 are
     u8 slot = 0;
     u8 level;
     u8 badgeCount = CountBadgesForOverworldWhiteOutLossCalculation();
+    u16 species;
     switch (area)
     {
     case WILD_AREA_LAND:
@@ -46,11 +49,21 @@ bool8 TryGenerateWildMonLevelScaling(const struct WildPokemonInfo * info, u8 are
         break;
     }
     level = ChooseWildMonLevel(&info->wildPokemon[slot]);
-    if (badgeCount == 0) level = 100;
+    if (badgeCount == 0) level = (Random() % 3) + 2;
+    else if (badgeCount == 1) level = (Random() % 6) + 7;
+    else if (badgeCount == 2) level = (Random() % 6) + 11;
+    else if (badgeCount == 3) level = (Random() % 6) + 15;
+    else if (badgeCount == 4) level = (Random() % 6) + 19;
+    else if (badgeCount == 5) level = (Random() % 6) + 23;
+    else if (badgeCount == 6) level = (Random() % 6) + 27;
+    else if (badgeCount == 7) level = (Random() % 6) + 31;
+    else if (badgeCount == 8) level = (Random() % 6) + 35;
+    species = info->wildPokemon[slot].species;
+    if (gEvolutionTable[species][0].method == EVO_LEVEL && gEvolutionTable[species][0].param <= level) species = gEvolutionTable[species][0].targetSpecies;
     if (flags == WILD_CHECK_REPEL && !IsWildLevelAllowedByRepel(level))
     {
         return FALSE;
     }
-    GenerateWildMon(info->wildPokemon[slot].species, level, slot);
+    GenerateWildMon(species, level, slot);
     return TRUE;
 }
